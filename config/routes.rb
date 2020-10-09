3e6aa60e34349095f27b52f8231083095fd5a44c
yuichi oyama <yuichi19991208@gmail.com>
2020-10-09 22:28:18 +0900

    最寄り駅のモデルとテーブルを作成し、アソシエーションを行った

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :properties
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'properties#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_092353) do
+ActiveRecord::Schema.define(version: 2020_10_09_132134) do
+
+  create_table "nearest_stations", force: :cascade do |t|
+    t.string "route_name"
+    t.string "station_name"
+    t.integer "time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "property_id"
+    t.index ["property_id"], name: "index_nearest_stations_on_property_id"
+  end
 
   create_table "properties", force: :cascade do |t|
     t.string "property_name"

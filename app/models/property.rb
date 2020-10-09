982c4cbce1018f5090cc2dbd39c5dbe016e86e52
yuichi oyama <yuichi19991208@gmail.com>
2020-10-09 22:41:12 +0900

    modelにvalidationを追加

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,9 @@
 class Property < ApplicationRecord
-  has_many :nearest_stations
+  has_many :nearest_stations, dependent: :destroy
+
+  validates :property_name, presence :true
+  validates :rent, presence :true
+  validates :address, presence :true
+  validates :age, presence :true
+  validates :note, presence :true
 end
